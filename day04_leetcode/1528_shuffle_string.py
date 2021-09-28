5f9ad4a29eafbe98d8669286364dfdb44e4cf23c
alsolex <alexeyvladimirovich38@gmail.com>
2021-09-28 14:54:33 +0300

    easy level day03

--- day04_leetcode/1528_shuffle_string.py
+++ day04_leetcode/1528_shuffle_string.py
@@ -1,8 +1,21 @@
+from typing import List
+
+
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         shuffled = dict(zip(indices, s))
         answer = ""
         for i in range(len(indices)):
-            answer += shuffled[i] 
+            answer += shuffled[i]
         return answer
-            
+
+
+def main():
+    s = input("Enter string: ")
+    indices = [int(idx) for idx in input("Enter indices: ").split()]
+    foo = Solution()
+    print("Answer: ", foo.restoreString(s, indices))
+
+
+if __name__ == "__main__":
+    main()
