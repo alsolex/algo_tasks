c155770f20ddbd091296b9c813e13435b1360fb5
alsolex <alexeyvladimirovich38@gmail.com>
2022-05-19 14:19:59 +0300

    add daily chalenge task amazing solution

---
+++ 329_Longest_Increasing_Path_in_a_Matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        matrix, length = (
+            {
+                i + j * 1j: val
+                for i, row in enumerate(matrix)
+                for j, val in enumerate(row)
+            },
+            {},
+        )
+        for z in sorted(matrix, key=matrix.get):
+            length[z] = 1 + max(
+                [
+                    length[Z]
+                    for Z in (z + 1, z - 1, z + 1j, z - 1j)
+                    if Z in matrix and matrix[z] > matrix[Z]
+                ]
+                or [0]
+            )
+        return max(length.values())
